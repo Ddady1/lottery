bd7613805a09f3099a54f945a755bce1dfaef1f5
dady <ddady1@gmail.com>
2022-08-03 14:58:53 +0300

    read file func

---
+++ lotto.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+def read_csv(file_path):
+
+    return pd.read_csv(file_path)
+    pass
+
+def all_times_stat():
+    pass
+
+def specific_date(date):
+    pass
+
+def dates_range(d_range):
+    pass
+
+def by_id(id_num):
+    pass
+
+
+
+
+
+
+
+#print(df.loc[df['date'] == '19/07/2022'])
+
+def main():
+
+    result_file = read_csv('lotto.csv')
+    print(result_file)
+
+if __name__ == '__main__':
+
+    main()
