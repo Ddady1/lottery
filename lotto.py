11161786904c6377459771bd1890df2c73fc7f0f
Dady <ddady1@gmail.com>
2022-08-04 23:14:57 +0300

    restart thinking

--- lotto.py
+++ lotto.py
@@ -1,20 +1,35 @@
 import pandas as pd
+import csv
 
 def read_csv(file_path):
 
     return pd.read_csv(file_path)
+
     pass
 
 def all_times_stat(file_to_dict):
 
     #print(file_to_dict['no_1'].value_counts())
+
+
+    #for i in range(len(file_to_dict)):
+    #    print(file_to_dict.loc[i, 'no_1'], file_to_dict.loc[i, 'no_2'])
+    dict_c = {}
     df_clean = file_to_dict.to_dict('dict')
     del df_clean['date']
     del df_clean['lottery_id']
-
-    #print(len(df_clean))
+    for key in df_clean:
+        #print(df_clean[key])
+        if df_clean[key][key] in dict_c:
+            temp = dict_c.get(key[key])
+            temp += 1
+            dict_c.update({key: temp})
+        else:
+            dict_c[key][key] = key
+    print(dict_c)
+       #print(len(df_clean))
     #print(df_clean['no_3'])
-    #print(df_clean)
+    #print(df_clean['no_1'])
     #df_clean = file_to_dict.loc[:, ~file_to_dict.columns.isin(['lottery_id', 'date'])]
 
 
