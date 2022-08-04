365759a44948236cdf17d63f550c133909db6bb0
dady <ddady1@gmail.com>
2022-08-04 15:22:11 +0300

    ststics

--- lotto.py
+++ lotto.py
@@ -5,7 +5,13 @@ def read_csv(file_path):
     return pd.read_csv(file_path)
     pass
 
-def all_times_stat():
+def all_times_stat(file_to_dict):
+
+    #print(file_to_dict['no_1'].value_counts())
+    #return file_to_dict.set_index('lottery_id').to_dict('list')
+    df_clean = file_to_dict.loc[:, ~file_to_dict.columns.isin(['lottery_id', 'date'])]
+    print(type(df_clean))
+
 
     pass
 
@@ -29,6 +35,9 @@ def by_id(id_num):
 def main():
 
     result_file = read_csv('lotto.csv')
+    dict_r = all_times_stat(result_file)
+    print(dict_r)
+    print(len(dict_r))
     #Uncheck the # in the print line in order to check that the function reads the correct file
     #print(result_file)
 
